9dee00f13256f05f535f0ba1c822ac575aca51d1
prithalllpinto <pintoprithal@gmail.com>
2026-08-23 12:22:05 +0530

    Initial commit - Payment Engine Simulator

---
+++ src/test/java/com/ecommerce/paymentengine/PaymentEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.paymentengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
